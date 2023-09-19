271e15ffab18e2f478209a730523ebb969d45f80
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-19 22:47:30 +0930

    adding

--- err2.c
+++ err2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int x;
-    printf("enter a number: ");
-    scanf("%d", &x);
-    printf("you entered: %d\n", x);
-    return 0;
+  int x;
+  printf("enter a number: ");
+  scanf("%d", &x);
+  printf("you entered: %d\n", x);
+  return 0;
 }
--- form_letter.c
+++ form_letter.c
@@ -7,8 +7,9 @@ int main(void) {
   int age;
   float employmentFraction;
 
-  printf("Entery your name, age, and employment fraction: ");
+  printf("Enter your name, age, and employment fraction: ");
   scanf("%10s %10s %d %f", firstName, lastName, &age, &employmentFraction);
+
   printf(
       "Your name is %s %s, you are %d years old, and your employment fraction "
       "is %.1f.\n",
