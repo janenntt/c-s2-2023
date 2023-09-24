d3f2b13ad5cddfbdd31f2b08ebb17b27eb097cef
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-25 03:15:25 +0930

    adding

--- process_ages.c
+++ process_ages.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int ages[10];
-    int count = 0;
-    int age, sum = 0, max = -1, min = 151;
+  int ages[10];
+  int count = 0;
+  int age, sum = 0, max = -1, min = 151;
 
-    printf("Enter up to 10 ages in years (-1 to finish):\n");
+  printf("Enter up to 10 ages in years (-1 to finish):\n");
 
-    while (count < 10) {
-        printf("Enter an age: ");
-        scanf("%d", &age);
+  while (count < 10) {
+    printf("Enter an age: ");
+    scanf("%d", &age);
 
-        if (age == -1) {
-            break;
-        }
-
-        if (age < 0 || age > 150) {
-            printf("Please enter a valid age between 0 and 150.\n");
-            continue;
-        }
-
-        ages[count] = age;
-        sum += age;
-
-        if (age > max) {
-            max = age;
-        }
-
-        if (age < min) {
-            min = age;
-        }
-
-        count++;
+    if (age == -1) {
+      break;
     }
 
-    if (count == 0) {
-        printf("No valid ages entered.\n");
-    } else {
-        float average = (float)sum / count;
-        printf("%d %d %.1f\n", max, min, average);
+    if (age < 0 || age > 150) {
+      printf("Please enter a valid age between 0 and 150.\n");
+      continue;
     }
 
-    return 0;
+    ages[count] = age;
+    sum += age;
+
+    if (age > max) {
+      max = age;
+    }
+
+    if (age < min) {
+      min = age;
+    }
+
+    count++;
+  }
+
+  if (count == 0) {
+    printf("No valid ages entered.\n");
+  } else {
+    float average = (float)sum / count;
+    printf("%d %d %.1f\n", max, min, average);
+  }
+
+  return 0;
 }
--- to_uppercase2.c
+++ to_uppercase2.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 int main() {
-    char name[21];  // Allocate space for 20 characters plus '\0'
-    int i = 0;
+  char name[21];  // Allocate space for 20 characters plus '\0'
+  int i = 0;
 
-    printf("Enter a string (up to 20 characters): ");
-    fgets(name, sizeof(name), stdin);
+  printf("Enter a string (up to 20 characters): ");
+  fgets(name, sizeof(name), stdin);
 
-    // Convert the string to uppercase
-    while (name[i] != '\0' && i < 20) {
-        name[i] = toupper(name[i]);
-        i++;
-    }
+  // Convert the string to uppercase
+  while (name[i] != '\0' && i < 20) {
+    name[i] = toupper(name[i]);
+    i++;
+  }
 
-    // Print the uppercase string
-    printf("%s\n", name);
+  // Print the uppercase string
+  printf("%s\n", name);
 
-    return 0;
+  return 0;
 }
