ecc63c243e0252f3937172bd6a194b44a5211e39
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-25 03:09:16 +0930

    adding

---
+++ odd_evens.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int nums[] = {11,3,9,7,6,10,13,17,2,8,3};
+    // declare two integer arrays for even and odd numbers
+    int evens[11];
+    int odds[11];
+    int count_evens = 0;
+    int count_odds = 0;
+    // interating through the nums array
+    for (int i=0;i<11;i++){
+        if (nums[i]%2==0){
+            evens[count_evens]=nums[i];
+            count_evens++; // if the number is even, add it to the evens array
+        } else {
+            odds[count_odds]=nums[i];
+            count_odds++; // if the number is odd, add it to the odds array
+        }
+    }
+    // iterating through the odds array and print out its elements
+    for (int i=0;i<count_odds;i++){
+        printf("%d ",odds[i]);
+    }
+    printf("\n");
+
+    // iterating through the evens array and print out its elements
+    for (int i=0;i<count_evens;i++){
+        printf("%d ",evens[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
---
+++ reverse_in_place.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char name[21];  // Allocate space for 20 characters plus '\0'
+    int i = 0;
+
+    printf("Enter a string (up to 20 characters): ");
+    fgets(name, sizeof(name), stdin);
+
+    // Convert the string to uppercase
+    while (name[i] != '\0' && i < 20) {
+        if (isalpha(name[i])) {
+            name[i] = toupper(name[i]);
+        }
+        i++;
+    }
+
+    // Print the uppercase string
+    printf("%s\n", name);
+
+    return 0;
+}
