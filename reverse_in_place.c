c5227321506c173ca2b168e58c895d02a31b3b6a
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-25 03:22:04 +0930

    adding

--- reverse_in_place.c
+++ reverse_in_place.c
@@ -2,23 +2,23 @@
 #include <string.h>
 
 int main() {
-    char str[21]; // Allocate space for 20 characters plus '\0'
-    int length, i, j;
+  char str[21];  // Allocate space for 20 characters plus '\0'
+  int length, i, j;
 
-    printf("Enter a string (up to 20 characters): ");
-    scanf("%20s", str);
+  printf("Enter a string (up to 20 characters): ");
+  scanf("%20s", str);
 
-    length = strlen(str);
+  length = strlen(str);
 
-    // Reverse the string in place
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
+  // Reverse the string in place
+  for (i = 0, j = length - 1; i < j; i++, j--) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+  }
 
-    // Print the reversed string
-    printf("%s\n", str);
+  // Print the reversed string
+  printf("%s\n", str);
 
-    return 0;
+  return 0;
 }
