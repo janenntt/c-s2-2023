835a89847c21a12ecf7d2a9d222ceb01a37b0fd7
Tram Nguyen <tramnguyenntt@student-10-201-03-096.wireless.adelaide.edu.au>
2023-10-02 15:04:05 +1030

    adding all

--- temperatures01.c
+++ temperatures01.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h> // for malloc
+#include <stdlib.h>  // for malloc
 
-int main(void){
-    int count;
-    printf("Enter number of temperatures: "); // Ask for the number of temperatures to read
-    scanf("%d",&count);
+int main(void) {
+  int count;
+  printf("Enter number of temperatures: ");  // Ask for the number of
+                                             // temperatures to read
+  scanf("%d", &count);
 
-    // Dynamically allocate memory for the temperatures
-    double *temperatures = (double *)malloc(count * sizeof(double));
+  // Dynamically allocate memory for the temperatures
+  double *temperatures = (double *)malloc(count * sizeof(double));
 
-    // Read the temperatures into the array
-    printf("Enter %d temperatures:\n", count);
-    for (int i = 0; i < count; i++) {
-        scanf("%lf", &temperatures[i]);
-    }
+  // Read the temperatures into the array
+  printf("Enter %d temperatures:\n", count);
+  for (int i = 0; i < count; i++) {
+    scanf("%lf", &temperatures[i]);
+  }
 
-    // Print the temperatures in reverse order with 1 decimal point of precision
-    for (int i = count - 1; i >= 0; i--) {
-        printf("%.1f ", temperatures[i]);
-    }
-    printf("\n");
+  // Print the temperatures in reverse order with 1 decimal point of precision
+  for (int i = count - 1; i >= 0; i--) {
+    printf("%.1f ", temperatures[i]);
+  }
+  printf("\n");
 
-    // Free the dynamically allocated memory
-    free(temperatures);
+  // Free the dynamically allocated memory
+  free(temperatures);
 
-    return 0;
+  return 0;
 }
